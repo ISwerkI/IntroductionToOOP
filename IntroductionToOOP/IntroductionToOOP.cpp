73a93d6b29fa41e8f81d13f5af48e3feb1d282a3
Danil <Danil@DESKTOP-99KGHPD>
2025-10-20 17:08:37 +1000

    Add project files.

---
+++ IntroductionToOOP/IntroductionToOOP.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Fraction
+{
+	int numerator;
+	int denominator;
+public:
+	//				Get/Set
+	int get_num()const
+	{
+		return numerator;
+	}
+	int get_denom()const
+	{
+		return denominator;
+	}
+	void set_num(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denom(int denominator)
+	{
+		this->denominator = denominator;
+	}
+	//				Constructor
+	Fraction()
+	{
+		numerator = {};
+		denominator = 1;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->numerator = numerator;
+		this->denominator = denominator;
+		cout << "NumDenomArgConstructor:\t" << this << endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	//				Destrctor
+	~Fraction()
+	{
+		cout << "Destrctor:\t" << this << endl;
+	}
+	//				Operators
+	Fraction& operator=(const Fraction& other)
+	{
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyAssigment:\t\t" << this << endl;
+		return *this;
+	}
+	//				Metods
+	void print()const
+	{
+		cout << numerator << "/" << denominator << endl;
+	}
+	void reduction()
+	{
+		int result;
+		while (true)
+		{
+			int i;
+			if (numerator & 2 == 0)
+			{
+				numerator /= 2;
+				i = 2;
+			}
+			else if (numerator & 3 == 0)
+			{
+				numerator /= 3;
+				i = 3;
+			}
+			else if (numerator & 5 == 0)
+			{
+				numerator /= 5;
+				i = 5;
+			}
+			else
+			{
+				result *= numerator;
+				break;
+			}
+		}
+	}
+};
+
+Fraction operator+(const Fraction& left, const Fraction& right);
+Fraction operator-(const Fraction& left, const Fraction& right);
+Fraction operator/(const Fraction& left, const Fraction& right);
+Fraction operator*(const Fraction& left, const Fraction& right);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Fraction A(2,3);
+	Fraction B(4, 3);
+	A.print();
+	B.print();
+	Fraction C;
+	C = B - A;
+	C.print();
+}
+
+Fraction operator+( Fraction& left, Fraction& right)
+{
+	Fraction result;
+	result.set_num(left.get_num() * right.get_denom() + right.get_num() * left.get_denom());
+	result.set_denom(left.get_denom() * right.get_denom());
+	return result;
+}
+Fraction operator-(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	result.set_num(left.get_num() * right.get_denom() - right.get_num() * left.get_denom());
+	result.set_denom(left.get_denom() * right.get_denom());
+	return result;
+}
+Fraction operator/(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	result.set_num(left.get_num() * right.get_denom());
+	result.set_denom(left.get_denom() * right.get_num());
+	return result;
+}
+Fraction operator*(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	result.set_num(left.get_num() * right.get_num());
+	result.set_denom(left.get_denom() * right.get_denom());
+	return result;
+}
